e4e1ef7e1c346c34c920d2a1909c592f70a30ba8
ThiagoxMedeiros <48658033+ThiagoxMedeiros@users.noreply.github.com>
2019-03-29 21:05:12 -0300

    Update pares.c

--- pares.c
+++ pares.c
@@ -11,11 +11,11 @@ int main(void){
     scanf("%d",&n2);
 // percorrer a distancia entre os numeros e imprimir os numeros pares entre eles
     while(n2>=n1){
-        cont= n2-1;
         resto=n2%2;    
         if(resto==0){
             printf("%f",resto);
             }
+        n2--;
     }
     
     
